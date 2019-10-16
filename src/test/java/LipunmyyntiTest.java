aacb85f7a15eb31bb91f3ab9477b64b8ded760fb
mviskari <viskarimatti@gmail.com>
2019-10-16 15:03:48 +0300

    uudemmat kujeet

--- src/test/java/LipunmyyntiTest.java
+++ src/test/java/LipunmyyntiTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LipunmyyntiTest {
-
+	
 	@Test
 	public void laskehinta() {
 		//fail("Not yet implemented");
@@ -13,11 +13,14 @@ public class LipunmyyntiTest {
 		Paasylippu olio2 = new Paasylippu(0, null);
 		Profiili olio3 = new Profiili();
 		
-		double tested = 75.0;
-		olio2.setHinta(75.0);
+		double hinta = 50.0;
+		double alennuspros = 25.0;
+		double tested = 12.50;
+		//olio2.setHinta(75.0);
 		//tested = olio2.getHinta(int);
-		assertEquals(tested, olio2.getHinta(),0.1);
+		assertEquals(tested, olio3.laskeAlennus(hinta,alennuspros),0.1);
 		
+		System.out.println(olio3.laskeAlennus(hinta,  alennuspros));
 	}
 
 }
